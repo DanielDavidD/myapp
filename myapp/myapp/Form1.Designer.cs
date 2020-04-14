161b8c4fb14cf78391f005157903e9f0169cf274
DanielDavidD <63288601+DanielDavidD@users.noreply.github.com>
2020-04-14 11:05:14 +0300

    Add files via upload

---
+++ myapp/myapp/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace myapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pb_Start = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pb_finish = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_finish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb1.Location = new System.Drawing.Point(0, 288);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(1497, 50);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb2.Image = ((System.Drawing.Image)(resources.GetObject("pb2.Image")));
+            this.pb2.Location = new System.Drawing.Point(992, 44);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(197, 251);
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb3.Image = ((System.Drawing.Image)(resources.GetObject("pb3.Image")));
+            this.pb3.Location = new System.Drawing.Point(1213, -1);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(339, 260);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pb_Start
+            // 
+            this.pb_Start.Image = ((System.Drawing.Image)(resources.GetObject("pb_Start.Image")));
+            this.pb_Start.Location = new System.Drawing.Point(0, 344);
+            this.pb_Start.Name = "pb_Start";
+            this.pb_Start.Size = new System.Drawing.Size(263, 71);
+            this.pb_Start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb_Start.TabIndex = 4;
+            this.pb_Start.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb4.Location = new System.Drawing.Point(0, -1);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(1215, 18);
+            this.pb4.TabIndex = 5;
+            this.pb4.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb5.Location = new System.Drawing.Point(844, 12);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(114, 247);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb5.TabIndex = 6;
+            this.pb5.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb6.Image = ((System.Drawing.Image)(resources.GetObject("pb6.Image")));
+            this.pb6.Location = new System.Drawing.Point(612, 44);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(200, 251);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb6.TabIndex = 7;
+            this.pb6.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb7.Image = ((System.Drawing.Image)(resources.GetObject("pb7.Image")));
+            this.pb7.Location = new System.Drawing.Point(404, 12);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(182, 260);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb7.TabIndex = 8;
+            this.pb7.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb8.Location = new System.Drawing.Point(233, 80);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(151, 215);
+            this.pb8.TabIndex = 9;
+            this.pb8.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb9.Location = new System.Drawing.Point(0, 12);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(412, 50);
+            this.pb9.TabIndex = 10;
+            this.pb9.TabStop = false;
+            // 
+            // pb10
+            // 
+            this.pb10.BackColor = System.Drawing.Color.RosyBrown;
+            this.pb10.Location = new System.Drawing.Point(0, 104);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(239, 191);
+            this.pb10.TabIndex = 11;
+            this.pb10.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 436);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 100);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "Maze made by Master Yobama";
+            // 
+            // pb_finish
+            // 
+            this.pb_finish.BackColor = System.Drawing.Color.Salmon;
+            this.pb_finish.Image = ((System.Drawing.Image)(resources.GetObject("pb_finish.Image")));
+            this.pb_finish.Location = new System.Drawing.Point(0, -1);
+            this.pb_finish.Name = "pb_finish";
+            this.pb_finish.Size = new System.Drawing.Size(130, 154);
+            this.pb_finish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_finish.TabIndex = 13;
+            this.pb_finish.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 576);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(328, 423);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(328, 423);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(258, 156);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1924, 631);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pb_finish);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pb10);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb_Start);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_finish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pb_Start;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pb_finish;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ myapp/myapp/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Click to make the transformation";
+            timer1.Start();
+            pb1.MouseEnter += new EventHandler(pb1_Enter);
+            pb2.MouseEnter += new EventHandler(pb2_Enter);
+            pb3.MouseEnter += new EventHandler(pb3_Enter);
+            pb4.MouseEnter += new EventHandler(pb4_Enter);
+            pb5.MouseEnter += new EventHandler(pb5_Enter);
+            pb6.MouseEnter += new EventHandler(pb6_Enter);
+            pb7.MouseEnter += new EventHandler(pb7_Enter);
+            pb8.MouseEnter += new EventHandler(pb8_Enter);
+            pb9.MouseEnter += new EventHandler(pb9_Enter);
+            pb10.MouseEnter += new EventHandler(pb10_Enter);
+            pb_Start.MouseEnter += new EventHandler(pbStart_Enter);
+            pb_finish.MouseEnter += new EventHandler(pbFinish_Enter);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pb_Start.Location = new Point(pb_Start.Location.X + 100, pb_Start.Location.Y);
+        }
+
+        private void pb1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You finished your journey as a Padawan");
+        }
+
+        private void pb2_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You fought hard and died as the last Jedi");
+        }
+
+        private void pb3_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You finished your journey as a Padawan");
+        }
+
+        private void pb4_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You finished your journey as a Padawan");
+        }
+
+        private void pb5_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You fought hard and died as the last Jedi");
+        }
+
+        private void pb6_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You didn't survive tha falling of the order" );
+        }
+
+        private void pb7_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You reached Baby Yoda's rank");
+        }
+
+        private void pb8_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You were so close to reaching Master Yoda,s rank");
+        }
+
+        private void pb9_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You were so close to reaching Master Yoda,s rank");
+        }
+
+        private void pb10_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You were so close to reaching Master Yoda,s rank");
+        }
+
+
+
+        private void pbFinish_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congratulations,your reached Yoda's rank");
+        }
+
+        private void pbStart_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wait until the start dissapears");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (pictureBox2.Visible == true)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+            }
+            else if (pictureBox1.Visible)
+            {
+                pictureBox2.Visible = true;
+                pictureBox1.Visible = false;
+            }
+                
+
+           
+
+        }
+    }
+}
